af02ce76237b6cbfa6e24fa695c6bae220946a7a
wulfman97 <calebschade97@gmail.com>
2015-08-20 17:22:54 -0600

    lunch lady finish

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,15 +1,56 @@
-puts "Welcome to the church and state building cafeteria! What would you like to order?"
+loop do
 
-@menu = { @entree:{ "Ramen" => 1, "Meatloaf" => 3, "Pizza" => 2}, @side:{"taters" => 1, "beans" => 1, "Mac & Cheese" => 2 } }
+  puts "Welcome to the church and state building cafeteria! What would you like to order? If you wish to quit then enter q for your entree and side."
 
-@menu.@entree.each do | item, price |
-  puts "#{item}: $#{price}"
+  @menu = { entree:{ "1. Ramen" => 1, "2. Meatloaf" => 3, "3. Pizza" => 2}, side:{"1. taters" => 1, "2.beans" => 1, "3. Mac & Cheese" => 2 } }
+
+  @menu[:entree].each do | item, price |
+    puts "#{item}: $#{price}"
+  end
+
+
+  print "Please select your entree: "
+  item = gets.chomp.strip.to_s.downcase
+
+
+  @menu[:side].each do | item, price |
+    puts "#{item}: $#{price}"
+  end
+
+  print "Please select your side: "
+  item_2= gets.chomp.strip.to_s
+
+  if item == '1' && item_2 == '1'
+   puts "Your total is: $2"
+
+  elsif item == '1'&& item_2 == '2'
+    puts "Your total is: $2"
+
+  elsif item == '1' && item_2 == '3'
+    puts "Your total is: $3" 
+
+  elsif item == '2' && item_2 == '1'
+   puts "Your total is: $4"
+
+  elsif item == '2' && item_2 == '2'
+    puts "Your total is: $4"
+
+  elsif item == '2' && item_2 == '3'
+    puts "Your total is: $5"
+
+  elsif item == '3' && item_2 == '1'
+   puts "Your total is: $3"
+
+  elsif item == '3' && item_2 == '2'
+    puts "Your total is: $3"
+
+  elsif item == '3' && item_2 == '3'
+    puts "Your total is: $4"
+
+  elsif item == "q" || item_2 == 'q'
+  else 
+    puts "I'm sorry, I didn't understand you"
+  end
+break if item == 'q' or item_2 == 'q'
 end
 
-
-print "Please pick your entree: "
-item = gets.chomp.strip.to_s
-
-@menu.@side.each do | item, price |
-  puts "#{item}: $#{price}"
-end
